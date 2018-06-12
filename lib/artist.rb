e83255857a0d3547386fcccca7530892e8e3326d
Adam Urbina <adamurbina@gmail.com>
2018-06-12 14:28:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,20 @@
 class Artist
+
+    attr_accessor :name,
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
     
+    def save
+        @@all << self
+    end
+
 end
