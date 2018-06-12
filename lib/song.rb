62e5fb9de0359f571b404645b9afd1ee0561a791
Adam Urbina <adamurbina@gmail.com>
2018-06-12 14:41:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
         @artist = artist
     end
 
-    def new_by_filename(file_name)
+    def self.new_by_filename(file_name)
         song_data = filename.split(" - ")
         song_title = song_data[1]
         song_artist = song_data[0]
