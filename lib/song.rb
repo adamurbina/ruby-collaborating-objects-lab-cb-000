a99b2873a561110b4a4cf028d209d5173171f996
Adam Urbina <adamurbina@gmail.com>
2018-06-12 15:22:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,7 @@ class Song
         #puts("new song name: #{new_song.name}")
         new_artist = Artist.find_or_create_by_name(song_artist)
         new_song.artist = new_artist
+        return new_song.name
         #puts("new song name2: #{new_song.name}")
     end
 end
