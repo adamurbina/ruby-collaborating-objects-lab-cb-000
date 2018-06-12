e5f069e47c3ac8b8cf3d06f389d781eae29a3cc3
Adam Urbina <adamurbina@gmail.com>
2018-06-12 15:41:54 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,12 +16,9 @@ class MP3Importer
 
     def import
         files
-        puts files
         @files.each{ |file|
             song = Song.new_by_filename(file)
         }
-        puts Artist.all.count
-        puts Artist.all.first.songs
     end
 
 end
