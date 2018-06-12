f6752533708f8442c332c7799c9b277edb963327
Adam Urbina <adamurbina@gmail.com>
2018-06-12 14:40:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ class Song
         song_title = song_data[1]
         song_artist = song_data[0]
         new_instance = self.new(song_title)
-        
+        artist = Artist.find_or_create_by_name(song_artist)
+        self.artist = artist
     end
 end
