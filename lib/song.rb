de5b2f1006f3dad1485ae436ab5c2e8b7085e856
Adam Urbina <adamurbina@gmail.com>
2018-06-12 15:16:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,22 +10,22 @@ class Song
 
     def self.new_by_filename(file_name)
         song_data = file_name.split(" - ")
-        puts song_data
+        #puts song_data
         song_artist = song_data[0]
-        puts("song artist: #{song_artist}")
+        #puts("song artist: #{song_artist}")
         song_title = song_data[1]
-        puts("song title: #{song_title}")
+        #puts("song title: #{song_title}")
 
         new_instance = Song.new(song_title)
-        puts ("new instance name1: #{new_instance.name}")
+        #puts ("new instance name1: #{new_instance.name}")
         artist = Artist.find_or_create_by_name(song_artist)
-        puts ("new instance name2: #{new_instance.name}")
+        #puts ("new instance name2: #{new_instance.name}")
         new_instance.artist = artist
-        puts ("new instance artist: #{new_instance.artist.name}")
+        #puts ("new instance artist: #{new_instance.artist.name}")
     end
 end
 
-filename = 'Michael Jackson - Black or White - pop.mp3'
+#filename = 'Michael Jackson - Black or White - pop.mp3'
 
 
 # song_data = filename.split(" - ")
@@ -35,7 +35,7 @@ filename = 'Michael Jackson - Black or White - pop.mp3'
 # puts ("song_title #{song_title}")
 # puts("-------")
 
-new_song = Song.new_by_filename(filename)
+#new_song = Song.new_by_filename(filename)
 # puts ("song name: #{new_song.name}")
 # puts ("song name: #{new_song.artist}")
 # puts ("song name: #{new_song.artist.name}")
